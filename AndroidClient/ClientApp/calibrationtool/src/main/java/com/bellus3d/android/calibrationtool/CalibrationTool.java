66d5412276f1581367d6d3243000da80f3b1eba2
Leon Feng <leon194@gmail.com>
2023-07-08 15:10:27 +0800

    Add nuvision feature DC-13

--- AndroidClient/ClientApp/calibrationtool/src/main/java/com/bellus3d/android/calibrationtool/CalibrationTool.java
+++ AndroidClient/ClientApp/calibrationtool/src/main/java/com/bellus3d/android/calibrationtool/CalibrationTool.java
@@ -3,8 +3,8 @@ package com.bellus3d.android.calibrationtool;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
-import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
 import android.os.Looper;
@@ -21,6 +21,7 @@ import com.bellus3d.android.arc.b3d4client.ConfigService.CameraConfigService;
 import com.bellus3d.android.arc.b3d4client.ConfigService.LocalConfigService;
 import com.bellus3d.android.arc.b3d4client.DiskService;
 import com.bellus3d.android.arc.b3d4client.ErrorReportingService.B3D4ClientError;
+import com.bellus3d.android.arc.b3d4client.FormattingService;
 import com.bellus3d.android.arc.b3d4client.LogService.LogService;
 import com.bellus3d.android.arc.b3d4client.ProcessingService;
 import com.bellus3d.android.arc.b3d4client.RecordingService;
@@ -28,14 +29,27 @@ import com.bellus3d.android.arc.b3d4client.camera.B3DCaptureSettings;
 import com.bellus3d.android.arc.b3d4client.camera.DepthCameraStreamListener;
 
 import java.io.File;
-import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.ARC_2M_COLOR_HEIGHT;
+import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.ARC_2M_COLOR_SIZE;
+import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.ARC_2M_COLOR_WIDTH;
+import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.ARC_2M_IR_HEIGHT;
+import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.ARC_2M_IR_SIZE;
+import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.ARC_2M_IR_WIDTH;
+import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.ARC_8M_COLOR_HEIGHT;
+import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.ARC_8M_COLOR_SIZE;
+import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.ARC_8M_COLOR_WIDTH;
+import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.ARC_8M_IR_SIZE;
 import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.ARC_APP_PATH;
 import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.EXP_LINE_TIME_2M;
 import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.EXP_LINE_TIME_8M;
 import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.NSTOMS;
 import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.OV8856_BINNING_FACTOR;
-import static com.bellus3d.android.arc.b3d4client.GlobalResourceService.TAG;
 import static com.bellus3d.android.arc.b3d4client.camera.B3DDepthCamera.CaptureEvent.*;
 
 
@@ -54,7 +68,10 @@ public class CalibrationTool extends AppCompatActivity{
 
     Button mCaptureBtn, mCamOpenBtn, mCamCloseBtn, mFloodBtn, mProjectorBtn;
 
-    Button mWifiOpenButton, mWifiCloseButton;
+    Button mNuCaptureButton, mWifiCloseButton;
+
+    private static AtomicBoolean nvCapture = new AtomicBoolean(false);
+    private static int mNVCaptureCount = 0;
 
     CheckBox ResSwitchchk;
 
@@ -220,72 +237,83 @@ public class CalibrationTool extends AppCompatActivity{
 
         mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 
-        mWifiOpenButton.setOnClickListener(new View.OnClickListener() {
-
+        mNuCaptureButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                wifiTestUtil = null;
-                wifiTestUtil = new WifiTestUtil(mWifiManager) {
-
-                    public void wifiStateChange(int newState) {
-                        LogService.d(TAG, "wifiStateChange newState="+newState);
-                        switch (newState) {
-                            case WifiManager.WIFI_STATE_ENABLED:
-                                mWifiState.setText("Wifi ON,Discovering...");
-                                mIs5GHzBandSupported = mWifiManager.is5GHzBandSupported();
-                                break;
-                            case WifiManager.WIFI_STATE_DISABLED:
-                                //tvWifiState.setText("Wifi OFF");
-                                break;
-                            case WifiManager.WIFI_STATE_DISABLING:
-                                mWifiState.setText("Wifi Closing");
-                                break;
-                            case WifiManager.WIFI_STATE_ENABLING:
-                                mWifiState.setText("Wifi Opening");
-                                break;
-                            case WifiManager.WIFI_STATE_UNKNOWN:
-                            default:
-                                mWifiState.setText("Wifi state Unknown");
-                                // do nothing
-                                break;
-
-                        }
-                    }
-
-                    public void wifiDeviceListChange(List<ScanResult> wifiDeviceList) {
-                        if (wifiDeviceList == null) {
-                            return;
-                        }
-
-                        //tvWifiDeviceList.setText("");
-                        LogService.d(TAG, "wifiDeviceListChange mIs5GHzBandSupported="+mIs5GHzBandSupported);
-                        boolean isfound = false;
-                        for (ScanResult result : wifiDeviceList) {
-                            LogService.d(TAG, "device name: " + result.SSID + ", signal level: " + String.valueOf(result.level));
-                            LogService.d(TAG, "result : " +result.toString());
-                            HostName = CameraConfigService.get("debug.wifitest.hostname", "Bellus3d");
-                            HostLevelThres = CameraConfigService.getInt("debug.wifitest.strenghlevel", -50);
-                            if(HostName.equals(result.SSID) && (HostLevelThres > result.level || result.level != 0)) {
-                                isfound = true;
-                                break;
-                            }
-                        }
-                        CameraConfigService.set("debug.wifitest.done", "1");
-                        if(isfound) {
-                            CameraConfigService.set("debug.wifitest.result", "1");
-                            mWifiState.setText("Wifi Test Pass");
-                        } else {
-                            CameraConfigService.set("debug.wifitest.result", "0");
-                            mWifiState.setText("Wifi Test Failed");
-                        }
-                        wifiTestUtil.stopTest();
-                    }
-                };
-                CameraConfigService.set("debug.wifitest.done", "0");
-                CameraConfigService.set("debug.wifitest.result", "0");
-                wifiTestUtil.startTest(CalibrationTool.this);
+            public void onClick(View view) {
+                if (!nvCapture.get()) {
+                    nvCapture.set(true);
+                    _recordingService.startFlood();
+                    mNVCaptureCount = 0;
+                }
             }
         });
+
+//        mWifiOpenButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                wifiTestUtil = null;
+//                wifiTestUtil = new WifiTestUtil(mWifiManager) {
+//
+//                    public void wifiStateChange(int newState) {
+//                        LogService.d(TAG, "wifiStateChange newState="+newState);
+//                        switch (newState) {
+//                            case WifiManager.WIFI_STATE_ENABLED:
+//                                mWifiState.setText("Wifi ON,Discovering...");
+//                                mIs5GHzBandSupported = mWifiManager.is5GHzBandSupported();
+//                                break;
+//                            case WifiManager.WIFI_STATE_DISABLED:
+//                                //tvWifiState.setText("Wifi OFF");
+//                                break;
+//                            case WifiManager.WIFI_STATE_DISABLING:
+//                                mWifiState.setText("Wifi Closing");
+//                                break;
+//                            case WifiManager.WIFI_STATE_ENABLING:
+//                                mWifiState.setText("Wifi Opening");
+//                                break;
+//                            case WifiManager.WIFI_STATE_UNKNOWN:
+//                            default:
+//                                mWifiState.setText("Wifi state Unknown");
+//                                // do nothing
+//                                break;
+//
+//                        }
+//                    }
+//
+//                    public void wifiDeviceListChange(List<ScanResult> wifiDeviceList) {
+//                        if (wifiDeviceList == null) {
+//                            return;
+//                        }
+//
+//                        //tvWifiDeviceList.setText("");
+//                        LogService.d(TAG, "wifiDeviceListChange mIs5GHzBandSupported="+mIs5GHzBandSupported);
+//                        boolean isfound = false;
+//                        for (ScanResult result : wifiDeviceList) {
+//                            LogService.d(TAG, "device name: " + result.SSID + ", signal level: " + String.valueOf(result.level));
+//                            LogService.d(TAG, "result : " +result.toString());
+//                            HostName = CameraConfigService.get("debug.wifitest.hostname", "Bellus3d");
+//                            HostLevelThres = CameraConfigService.getInt("debug.wifitest.strenghlevel", -50);
+//                            if(HostName.equals(result.SSID) && (HostLevelThres > result.level || result.level != 0)) {
+//                                isfound = true;
+//                                break;
+//                            }
+//                        }
+//                        CameraConfigService.set("debug.wifitest.done", "1");
+//                        if(isfound) {
+//                            CameraConfigService.set("debug.wifitest.result", "1");
+//                            mWifiState.setText("Wifi Test Pass");
+//                        } else {
+//                            CameraConfigService.set("debug.wifitest.result", "0");
+//                            mWifiState.setText("Wifi Test Failed");
+//                        }
+//                        wifiTestUtil.stopTest();
+//                    }
+//                };
+//                CameraConfigService.set("debug.wifitest.done", "0");
+//                CameraConfigService.set("debug.wifitest.result", "0");
+//                wifiTestUtil.startTest(CalibrationTool.this);
+//            }
+//        });
 
 //        mWifiCloseButton.setOnClickListener(new View.OnClickListener() {
 //
@@ -313,11 +341,14 @@ public class CalibrationTool extends AppCompatActivity{
         mCamCloseBtn.setClickable(false);
         mCaptureBtn.setClickable(false);
 
-        mWifiOpenButton = (Button) findViewById(R.id.WifiOpenButton);
+        mNuCaptureButton = (Button) findViewById(R.id.NuVCapture);
 
         mWifiState = (TextView) findViewById(R.id.wifi_state_content);
 
         mFPS = (TextView) findViewById(R.id.fps_state_content);
+
+        mWifiState.setVisibility(View.INVISIBLE);
+        mFPS.setVisibility(View.INVISIBLE);
 
         ResSwitchchk    = (CheckBox) findViewById(R.id.ResSwitchchk);
 
@@ -391,7 +422,74 @@ public class CalibrationTool extends AppCompatActivity{
 
         @Override
         public void onFrameNative(byte[] byteData, long mTotalFramesStreamed, long currentTimeStamp, int mResolution, boolean isCapture) {
-            _processingService.addFrameToNativeJNI(byteData,mTotalFramesStreamed,currentTimeStamp,mResolution,isCapture);
+            final int coloe_height = ResSwitchchk.getText().equals("8M") ? ARC_8M_COLOR_HEIGHT : ARC_2M_COLOR_HEIGHT;
+            final int coloe_width = ResSwitchchk.getText().equals("8M") ? ARC_8M_COLOR_WIDTH : ARC_2M_COLOR_WIDTH;
+            final int ir_height = ARC_2M_IR_HEIGHT;
+            final int ir_width = ARC_2M_IR_WIDTH;
+            final int color_size = (int) (coloe_height * coloe_width * 1.5);
+            final int ir_size = ir_height * ir_width;
+            if(nvCapture.get()) {
+                if(mNVCaptureCount == 3) {
+                    _recordingService.stopFlood();
+                    _recordingService.startProjector();
+                    final byte[] frameData = byteData.clone();
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            byte[] colorFrame = FormattingService.compressYuvImg(Arrays.copyOfRange(frameData, 0, coloe_height * coloe_width * 3 / 2),
+                                    ImageFormat.NV21, coloe_width, coloe_height, null, "JPEG");
+                            String colorPath = "/sdcard/Bellus3d/Arc/calibrationtool/M/flood_M.jpg";
+                            try (FileOutputStream fos = new FileOutputStream(colorPath)) {
+                                fos.write(colorFrame);
+                                //fos.close(); There is no more need for this line since you had created the instance of "fos" inside the try. And this will automatically close the OutputStream
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            String irLPath = "/sdcard/Bellus3d/Arc/calibrationtool/L/flood_L.jpg";
+                            byte[] byteToSave = Arrays.copyOfRange(frameData, color_size, color_size + ir_size);
+                            FormattingService.savePNG8UC1(byteToSave, ir_width, ir_height, irLPath);
+
+                            String irRPath = "/sdcard/Bellus3d/Arc/calibrationtool/R/flood_R.jpg";
+                            byteToSave = Arrays.copyOfRange(frameData, color_size + ir_size, color_size + ir_size * 2);
+                            FormattingService.savePNG8UC1(byteToSave, ir_width, ir_height, irRPath);
+                        }
+                    }).start();
+                } else if(mNVCaptureCount == 7) {
+                    final byte[] frameData = byteData.clone();
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            byte[] colorFrame = FormattingService.compressYuvImg(Arrays.copyOfRange(frameData, 0, coloe_height * coloe_width * 3 / 2),
+                                    ImageFormat.NV21, coloe_width, coloe_height, null, "JPEG");
+                            String colorPath = "/sdcard/Bellus3d/Arc/calibrationtool/M/prj_M.jpg";
+                            try (FileOutputStream fos = new FileOutputStream(colorPath)) {
+                                fos.write(colorFrame);
+                                //fos.close(); There is no more need for this line since you had created the instance of "fos" inside the try. And this will automatically close the OutputStream
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            String irLPath = "/sdcard/Bellus3d/Arc/calibrationtool/L/prj_L.jpg";
+                            byte[] byteToSave = Arrays.copyOfRange(frameData, color_size, color_size + ir_size);
+                            FormattingService.savePNG8UC1(byteToSave, ir_width, ir_height, irLPath);
+
+                            String irRPath = "/sdcard/Bellus3d/Arc/calibrationtool/R/fprj_R.jpg";
+                            byteToSave = Arrays.copyOfRange(frameData, color_size + ir_size, color_size + ir_size * 2);
+                            FormattingService.savePNG8UC1(byteToSave, ir_width, ir_height, irRPath);
+                        }
+                    }).start();
+                    _recordingService.stopProject();
+                    nvCapture.set(false);
+                }
+                mNVCaptureCount++;
+            } else {
+                _processingService.addFrameToNativeJNI(byteData,mTotalFramesStreamed,currentTimeStamp,mResolution,isCapture);
+            }
             //            if(depthCamera.getCaptureMilliseconds() != 0.0) {
 //                final float fps = (float) depthCamera.getCapturedFrames() / (float) depthCamera.getCaptureMilliseconds() * 1000;
 //                runOnUiThread(new Runnable() {
